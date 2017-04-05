cdab8ca0bd5d5902fdf7803e976b930066077ef2
Ravish Gupta <gupta269@pnw.edu>
2017-04-04 22:16:20 -0500

    update sider crawler to contain CIDs (concept_ids) #41 #19

--- tensoflow_w2v/sider_crawl/sider_crawler.py
+++ tensoflow_w2v/sider_crawl/sider_crawler.py
@@ -7,6 +7,7 @@ import re
 # css selector for sider to get side effects and indications
 TABLE_SELECTOR = '#drugInfoTable'
 TERM_SELECOTR = "table tr td a[href^='/se']"
+MULTIPLE_OP = "div ul li a[href^='drugs/']"
 
 
 def get_med_names():
@@ -14,11 +15,19 @@ def get_med_names():
     read the 100 medicine names from file
     """
     names = list()
+    # with open('missing_meds.txt', 'r') as f:
     with open('generic_medlist.txt', 'r') as f:
         for line in f:
             names.append(line.strip().lower())
     return names
 
+def get_terms_cids(table):
+    terms_cid = dict()
+    for term in table.find_elements_by_css_selector(TERM_SELECOTR):
+        url = term.get_attribute("href")
+        clean_term_text = clean_text(term.text)
+        terms_cid[clean_term_text] = url.rsplit('/', 2)[-2]
+    return terms_cid
 
 def get_terms(table):
     """
@@ -51,14 +60,25 @@ for name in med_names:
     # put the medicine name in the search box
     elem.send_keys(name)
     elem.send_keys(Keys.RETURN)
+    # Check if autoselect page.
+    auto_comp_option = driver.find_elements_by_css_selector(MULTIPLE_OP)
+    if auto_comp_option:
+        for options in auto_comp_option:
+            if options.text == name:
+                options.click()
+                break
+
     driver.implicitly_wait(10)  # wait until the site is fully rendered
     # there are two tables, one for side effects and one for indications
     se_tables = driver.find_elements_by_css_selector(TABLE_SELECTOR)
     if se_tables:
-        side_effects = get_terms(se_tables[0])
-        indications = get_terms(se_tables[1])
+        #side_effects = get_terms(se_tables[0])
+        side_effects = get_terms_cids(se_tables[0])
+        #indications = get_terms(se_tables[1])
+        indications = get_terms_cids(se_tables[1])
         side_effects_cl = []
         indications_cl = []
+        '''
         if indications:
             for ind in indications:
                 ind = clean_text(ind)
@@ -67,10 +87,10 @@ for name in med_names:
             for se in side_effects:
                 se = clean_text(se)
                 side_effects_cl.append(se)
-
+        '''
         med_name_cl = clean_text(name)
         final_results[med_name_cl] = {
-            'side_effects': side_effects_cl, 'indications': indications_cl}
+            'side_effects': side_effects, 'indications': indications}
     else:
         med_not_found.append(name)
 
@@ -78,5 +98,5 @@ driver.close()
 print("Generic Medicine not in SIDER")
 print(med_not_found)
 # write result into json file
-with open('med_se_in_cleaned.json', 'w') as f:
+with open('only_effs_v1.json', 'w') as f:
     json.dump(final_results, f, indent=2)
